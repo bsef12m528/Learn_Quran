8b28e05bbc5f9c11ec6044454c0971d7e1d465bc
bsef12m528 <bsef12m528@pucit.edu.pk>
2015-06-14 22:36:14 +0500

    my first pushing

---
+++ app/src/main/java/com/project/learnquran/Lessons.java
@@ -0,0 +1,94 @@
+package com.project.learnquran;
+
+import java.io.IOException;
+import java.net.URI;
+
+import android.R.string;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Lessons extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lessons);
+	}
+	public void lessonNameHighlightNotification(int index,int prev){
+		FragmentManager fm = getFragmentManager(); //if added by xml
+		Lessonnames fragment = (Lessonnames) fm.findFragmentById(R.id.fragment_lessons_names);
+		fragment.highLightButton(index,prev);
+	}
+	public void changeContentNotification(int index){
+		FragmentManager fm = getFragmentManager(); //if added by xml
+		Contents fragment = (Contents) fm.findFragmentById(R.id.fragment_contents);
+		fragment.loadScreen(index);
+	}
+    public static MediaPlayer mp = null;
+	public void play(View v) {
+		Toast.makeText(getApplicationContext(), v.getContentDescription() +"" , Toast.LENGTH_SHORT).show();
+		AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+		// For example to set the volume of played media to maximum.
+		audioManager.setStreamVolume (AudioManager.STREAM_MUSIC, audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),0);
+		if(mp == null){
+			mp = new MediaPlayer();
+			AssetFileDescriptor afd;
+			try {
+				String name = v.getContentDescription().toString();
+				name+=".mp3";
+				afd=getAssets().openFd(name);
+				mp.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+				mp.prepare();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			mp.start();
+		}
+		else{
+			if(mp.isPlaying())
+			{
+				mp.stop();
+				mp.reset();
+				AssetFileDescriptor afd;
+	            try {
+	            	String name = v.getContentDescription().toString();
+					name+=".mp3";
+					afd=getAssets().openFd(name);
+					mp.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+					mp.prepare();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	            mp.start();
+			}
+			else
+			{
+				mp.reset();
+				AssetFileDescriptor afd;
+	            try {
+	            	String name = v.getContentDescription().toString();
+					name+=".mp3";
+					afd=getAssets().openFd(name);
+					mp.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+					mp.prepare();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	            mp.start();
+			}
+		}
+	}
+}
